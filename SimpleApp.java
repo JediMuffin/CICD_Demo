de0c2136374526e7ba06cac7f9d033938ca57d5f
Vince Ryan Bufete <33220919+JediMuffin@users.noreply.github.com>
2023-08-26 11:26:21 +0800

    Update SimpleApp.java

--- SimpleApp.java
+++ SimpleApp.java
@@ -1,5 +1,6 @@
 public class SimpleApp {
     public static void main(String[] args) {
         System.out.println("Hello, this is a simple 1 Java console application!");
+        System.out.println("test merge");
     }
 }
